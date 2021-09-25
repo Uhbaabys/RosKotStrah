0fe8e559f43fdfb68875226449ca8982935a7471
lenivoe <levishok@gmail.com>
2021-09-25 19:09:23 +0700

    test db

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('RosKotStrah', 'admin', 'admin', {
+    host: 'localhost',
+    dialect: 'postgres',
+    port: '5432'
+  });
+
+// const sequelize = new Sequelize('postgres://admin:admin:5432/RosKotStrah')
+
+  (async() => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+  })()
+

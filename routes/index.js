8056f116ba364901899c7596982ce8fee0211bb2
lenivoe <levishok@gmail.com>
2021-10-30 19:37:58 +0700

    animal type http requests: get all, create one

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
-const { sequelize, AnimalType } = require('../src/db');
+const { sequelize, AnimalType } = require('../src/db/db');
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'RosKotStrah' });
 });
 
-router.post('/', async (req, res, next) => {
+router.get('/animal', async (req, res, next) => {
+  const animalType = await sequelize.query('SELECT id, name FROM "Animal_Type";', {
+    model: AnimalType,
+    mapToModel: true,
+  });
+  res.json({ animals: animalType });
+});
+
+router.post('/animal', async (req, res, next) => {
   if (!req.body) {
     console.log('========= empty body =========>', req.headers);
   }
-  const { login, password } = req.body;
-  const name = `${login}:${password}`;
+  const { name } = req.body;
   // эквивалентно: const animalType = await AnimalType.create({ name });
-  const [animalType] = await sequelize.query(`INSERT INTO "Animal_Type" (name) VALUES ('${name}') RETURNING id, name;`, {
+  const [animalType] = await sequelize.query('INSERT INTO "Animal_Type" (name) VALUES (:name) RETURNING id, name;', {
+    replacements: { name },
     model: AnimalType,
     mapToModel: true,
   });
